3693827297aa7795a08e07ca4ff237748b0cdd65
thephm <accounts@ownmail.net>
2026-01-02 16:20:26 -0500

    Remove problematic sync_images_column migration

--- server/prisma/migrations/20260101201138_sync_images_column/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "roasters" ADD COLUMN     "images" TEXT[];
-
--- AddForeignKey
-ALTER TABLE "reviews" ADD CONSTRAINT "reviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
